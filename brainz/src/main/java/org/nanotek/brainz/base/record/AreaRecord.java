c63d805bd1a45d558703fe3ef6bfd7f14c527206
Jose Carlos Canova <jose.c.canova@gmail.com>
2024-09-24 15:06:48 -0300

    Update area model

--- brainz/src/main/java/org/nanotek/brainz/base/record/AreaRecord.java
+++ brainz/src/main/java/org/nanotek/brainz/base/record/AreaRecord.java
@@ -2,12 +2,20 @@ package org.nanotek.brainz.base.record;
 
 import java.util.UUID;
 
+import org.nanotek.brainz.base.entity.immutables.AreaEntity;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-public record AreaRecord(Long areaId , UUID gid , String areaName) {
+public record AreaRecord(Long areaId , UUID gid , String areaName) 
+implements AreaEntity{
 	
-	public AreaRecord(Long areaId , UUID gid , String areaName) {
+	@JsonCreator
+	public AreaRecord(@JsonProperty(value="areaId")Long areaId ,
+				@JsonProperty(value="gid")	UUID gid ,
+				@JsonProperty(value="areaName")   String areaName) {
 		this.areaId = areaId; 
 		this.gid = gid;
 		this.areaName=areaName;
